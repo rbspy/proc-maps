7cea714c098246a8051e444c55bd665924d78b91
Adam Jensen <adam@acj.sh>
2023-12-31 08:05:23 -0500

    CI: Remove unused caching from FreeBSD build script

--- ci/scripts/freebsd-build.sh
+++ ci/scripts/freebsd-build.sh
@@ -6,14 +6,5 @@ source "$HOME/.cargo/env"
 
 cd /vagrant
 
-if [ -f build-artifacts.tar ]; then
-  tar xf build-artifacts.tar
-  rm -f build-artifacts.tar
-fi
-
 cargo build --no-default-features --release --workspace --all-targets
 cargo test --no-default-features --release
-
-tar cf build-artifacts.tar target
-tar rf build-artifacts.tar .cargo/git || true
-tar rf build-artifacts.tar .cargo/registry || true
