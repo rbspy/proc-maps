dc4d42c2bacea5890ed54ec1154883b762c5ee7b
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-02-18 13:01:59 +0000

    ptrace.rs: fix build on FreeBSD ARM / POWER
    ARM and POWER use unsigned char.

--- src/freebsd_maps/ptrace.rs
+++ src/freebsd_maps/ptrace.rs
@@ -185,7 +185,7 @@ pub fn detach(pid: Pid) -> io::Result<()> {
 
 /// Read virtual memory entry
 pub fn read_vm_entry(pid: Pid, vm_entry: ptrace_vm_entry) -> io::Result<ptrace_vm_entry> {
-    let result = unsafe { ptrace(PT_VM_ENTRY, pid, &vm_entry as *const _ as *mut i8, 0) };
+    let result = unsafe { ptrace(PT_VM_ENTRY, pid, &vm_entry as *const _ as *mut c_char, 0) };
 
     if result == -1 {
         Err(io::Error::last_os_error())
