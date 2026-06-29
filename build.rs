450f6eaa963c1e68bc8ed2eb2d50ad3a159277a9
Adam Jensen <adam@acj.sh>
2026-06-28 21:43:35 -0400

    Fix deprecation warning

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
 
         let bindings = bindgen::Builder::default()
             .header("src/freebsd_maps/wrapper.h")
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
             .generate()
             .expect("Unable to generate bindings");
 
